fd5b7967c7d81524185adfb144fc4d15d732a9b9
gustavofinotti1234-collab <gustavo.finotti.1234@gmail.com>
2026-08-03 11:42:28 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+window.STUDIO_CONFIG = {
+  supabaseUrl: "https://jrnlphpccurlmpxvmnmi.supabase.co",
+  supabaseAnonKey: "sb_publishable_v-xcCtWcYeSz7M25-RihJw_6X64iQbY",
+  studioWhatsapp: "5517996558867",
+  timezone: "America/Sao_Paulo"
+};
